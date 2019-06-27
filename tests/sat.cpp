367d795169838649426942f21b2e32377814bf1f
Heinz Riener <heinz.riener@gmail.com>
2019-06-27 16:29:58 +0200

    more tests.

--- tests/sat.cpp
+++ tests/sat.cpp
@@ -89,3 +89,59 @@ TEMPLATE_TEST_CASE("Incremental", "[sat][template]", solver<solvers::glucose_41>
 	r = solver.solve(std::vector({t2}));
 	CHECK(r == result::states::unsatisfiable);
 }
+
+TEMPLATE_TEST_CASE("Model", "[sat][template]", solver<solvers::glucose_41>, solver<solvers::ghack>,
+                   solver<solvers::maple>)
+{
+	TestType solver;
+
+	/* a and b have equal values */
+	auto const a = lit_type(solver.add_variable(), lit_type::polarities::positive);
+	auto const b = lit_type(solver.add_variable(), lit_type::polarities::positive);
+	auto const t0 = add_tseitin_equals(solver, a, b);
+
+	auto r = solver.solve({t0});
+	CHECK(r == result::states::satisfiable);
+
+	auto m = solver.get_model();
+	auto model = m.model();
+	CHECK(model.at(t0.variable()) == lbool_type::true_);
+	CHECK(model.at(a.variable()) == model.at(b.variable()));
+
+	/* a and b have un-equal values */
+	auto const t1 = add_tseitin_xor(solver, a, b);
+
+	r = solver.solve({t1});
+	CHECK(r == result::states::satisfiable);
+
+	m = solver.get_model();
+	model = m.model();
+	CHECK(model.at(t1.variable()) == lbool_type::true_);
+	CHECK(model.at(a.variable()) != model.at(b.variable()));
+
+	/* a and b are both true */
+	auto const t2 = add_tseitin_and(solver, a, b);
+
+	r = solver.solve({t2});
+	CHECK(r == result::states::satisfiable);
+
+	m = solver.get_model();
+	model = m.model();
+	CHECK(model.at(t2.variable()) == lbool_type::true_);
+	CHECK(model.at(a.variable()) == lbool_type::true_);
+	CHECK(model.at(b.variable()) == lbool_type::true_);
+
+	/* at least one of a and b is true */
+	auto const t3 = add_tseitin_or(solver, a, b);
+
+	r = solver.solve({t3});
+	CHECK(r == result::states::satisfiable);
+
+	m = solver.get_model();
+	model = m.model();
+	CHECK(model.at(t3.variable()) == lbool_type::true_);
+
+	bool const a_or_b_is_true = model.at(a.variable()) == lbool_type::true_
+	                            | model.at(b.variable()) == lbool_type::true_;
+	CHECK(a_or_b_is_true);
+}
