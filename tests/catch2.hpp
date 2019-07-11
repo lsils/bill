e23c2a5168cd8b503aacd5c9e7bfef8c8985ab4c
Heinz Riener <heinz.riener@gmail.com>
2019-07-11 13:49:57 +0200

    windows warnings.

--- tests/catch2.hpp
+++ tests/catch2.hpp
@@ -7,7 +7,7 @@
 
 #if defined(BILL_WINDOWS_PLATFORM)
 #pragma warning(push)
-#pragma warning(disable : 4365 4514 4571 4583 4619 4623 4625 4626 4710 4711 4774 4820 4820 5026 5027 5039)
+#pragma warning(disable : 4365 4514 4571 4583 4619 4623 4625 4626 4710 4711 4774 4820 4820 4868 5026 5027 5039)
 #include <catch.hpp>
 #pragma warning(pop)
 #else
