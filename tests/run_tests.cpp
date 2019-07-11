197d90568071f4ff885f233eb3f8b94a2b4d542e
Heinz Riener <heinz.riener@gmail.com>
2019-07-11 13:24:22 +0200

    windows warnings.

--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -9,7 +9,8 @@
 #include <bill/utils/platforms.hpp>
 
 #if defined(BILL_WINDOWS_PLATFORM)
-#pragma warning(push, 0)
+#pragma warning(push)
+#pragma warning(disable : 4365 4514 4571 4583 4619 4623 4625 4626 4710 4711 4774 4820 4820 5026 5027 5039)
 #include <catch.hpp>
 #pragma warning(pop)
 #else
