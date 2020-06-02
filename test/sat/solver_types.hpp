854be46fbc5f95e3cc1db798c0ca5dff46e5f5ed
Siang-Yun (Sonia) Lee <lee30sonia@gmail.com>
2020-06-02 17:26:21 +0200

    push/pop & set_random_phase (#34)
    * push/pop & set_random_phase
    
    * test case
    
    * indentation
    
    * docs

--- test/sat/solver_types.hpp
+++ test/sat/solver_types.hpp
@@ -3,14 +3,18 @@
 
 #if defined(BILL_WINDOWS_PLATFORM) && defined(BILL_HAS_Z3)
 #define SOLVER_TYPES bill::solver<bill::solvers::glucose_41>, bill::solver<bill::solvers::ghack>, bill::solver<bill::solvers::bsat2>, bill::solver<bill::solvers::z3>
+#define STACKABLE_SOLVER_TYPES bill::solver<bill::solvers::bsat2>, bill::solver<bill::solvers::z3>
 #elif defined(BILL_WINDOWS_PLATFORM) && !defined(BILL_HAS_Z3)
 #define SOLVER_TYPES bill::solver<bill::solvers::glucose_41>, bill::solver<bill::solvers::ghack>, bill::solver<bill::solvers::bsat2>
+#define STACKABLE_SOLVER_TYPES bill::solver<bill::solvers::bsat2>
 #elif !defined(BILL_WINDOWS_PLATFORM) && defined(BILL_HAS_Z3)
 #define SOLVER_TYPES                                                                                               \
 	bill::solver<bill::solvers::glucose_41>, solver<bill::solvers::ghack>, bill::solver<bill::solvers::maple>, \
 	    bill::solver<bill::solvers::bsat2>, solver<bill::solvers::bmcg>, bill::solver<bill::solvers::z3>
+#define STACKABLE_SOLVER_TYPES bill::solver<bill::solvers::bsat2>, bill::solver<bill::solvers::z3>
 #else
 #define SOLVER_TYPES                                                                                                     \
 	bill::solver<bill::solvers::glucose_41>, bill::solver<bill::solvers::ghack>, bill::solver<bill::solvers::maple>, \
 	    bill::solver<bill::solvers::bsat2>, bill::solver<bill::solvers::bmcg>
+#define STACKABLE_SOLVER_TYPES bill::solver<bill::solvers::bsat2>
 #endif
